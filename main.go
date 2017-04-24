a764b88dbe52803c63da9c8d7511a722bdea8815
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2017-04-24 14:15:22 +0100

    Finished help command. Added command to change prefix, rule34, basic (and ugly) ibsearch commands, as well as info command to display some info about the bot

--- main.go
+++ main.go
@@ -13,27 +13,51 @@ import (
 	"io/ioutil"
 	"github.com/bwmarrin/discordgo"
 	"time"
-)
-
-var (
-	Token string
-	emojiRegex = regexp.MustCompile("<:.*?:(.*?)>")
- 	userIDRegex = regexp.MustCompile("<@!?([0-9]*)>")
- 	reply = map[string]string{
-		"kiddo": "mulia vs lulia :thinking:",
-		"censored": "stuff",}
-	commandList = []string{"bigMoji","userStats","setGame","help"}
+	"sort"
+	"math/rand"
+	"encoding/xml"
+	"strconv"
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Config struct {
 	Game string `json:"game"`
+	Prefix string `json:"prefix"`
 } 
 
+type Rule34 struct {
+	PostCount  int `xml:"count,attr"`
+	Posts	   []struct {
+		URL string `xml:"file_url,attr"`
+	} `xml:"post"`	
+}
+
 var c *Config
+var r34 Rule34
+
+var (
+	lastReboot string
+	Token string
+	emojiRegex = regexp.MustCompile("<:.*?:(.*?)>")
+ 	userIDRegex = regexp.MustCompile("<@!?([0-9]*)>")
+
+	commandList = []string{"bigMoji","userStats","help","r34","info","ibsearch"}
+	helpText = map[string]string{
+		"bigMoji [emoji]":"Sends a large version of the emoji as an image.\nShorthand available by excluding 'bigMoji'",
+		"userStats [user]":"Sends some basic details of the given user. \nIf no [user] is supplied, the command callers details are shown instead.",	
+		"help":"Prints this useful help text :D",
+		"r34 [search]":"For all your saucy needs",
+		"info":"Sends some basic details of 2Bot",
+		"ibsearch":"even saucier",
+	}
+	helpKeys = []string{}
+)
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
+	timeNow := time.Now()
+	lastReboot = timeNow.Format(time.RFC1123)[:22]
 }
 
 func main() {
@@ -43,6 +67,11 @@ func main() {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
+
+	for k := range helpText {
+		helpKeys = append(helpKeys, k)
+	}
+	sort.Strings(helpKeys)
 
 	//Register event handlers
 	dg.AddHandler(messageCreate)
@@ -56,168 +85,325 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+	defer dg.Close()
 
-
-	
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
 }
 
-func LoadConfig(s *discordgo.Session) {
-	_,err1 := s.UserUpdate("","","2Bot",s.State.User.Avatar,""); if err1 != nil {
-		fmt.Println(err1)
-	}
-	file, err := ioutil.ReadFile("config.json")
-	if err != nil {
-		fmt.Println("File error")
-	}
+func loadConfig(s *discordgo.Session) {
+	file, err := ioutil.ReadFile("config.json"); if err != nil { fmt.Println("Open config file error"); return }
 	json.Unmarshal(file, &c)
+
+	if c.Prefix == ""{
+		c.Prefix = "!owo"
+		err := saveConfig(); if err != nil { fmt.Println("Save config file error"); return }
+	}
 	s.UpdateStatus(0, c.Game)
 }
 
-func SaveConfig() error {
+func randRange(min, max int) int {
+    rand.Seed(time.Now().Unix())
+	if max == 0 {
+		return 0
+	}
+    return rand.Intn(max - min) + min
+}
+
+func getCreationTime(ID string) (t time.Time, err error) {
+    i, err := strconv.ParseInt(ID, 10, 64)
+    if err != nil {
+        return
+    }
+    timestamp := (i >> 22) + 1420070400000
+    t = time.Unix(timestamp/1000, 0)
+    return
+}
+
+func saveConfig() error {
 	out, err := json.Marshal(&c)
 	if err != nil {
 		return err
 	}
-	err1 := ioutil.WriteFile("config.json", out, 0777)
-	if err != nil {
-		return err1
-	}
+	err1 := ioutil.WriteFile("config.json", out, 0777); if err != nil {	return err1	}
 	return nil
 }
 
-func messageCreate(s *discordgo.Session, event *discordgo.MessageCreate) {
-	if strings.HasPrefix(event.Content, "!xd ") {
+func codeSeg(s ...string) string {
+	ret := "`"
+	for _, i := range s {
+		ret += i
+	}
+	return ret+"`"
+}
 
+func codeBlock(s ...string) string {
+	ret := "```"
+	for _, i := range s {
+		ret += i
+	}
+	return ret+"```"
+}
+
+//
+func emojiFile(s string) string {
+	found := ""
+	filename := ""
+	for _, r := range s {
+		if filename != "" {
+			filename = fmt.Sprintf("%s-%x", filename, r)
+		} else {
+			filename = fmt.Sprintf("%x", r)
+		}
+
+		if _, err := os.Stat(fmt.Sprintf("emoji/%s.png", filename)); err == nil {
+			found = filename
+		} else if found != "" {
+			return found
+		}
+	}
+	return found
+}
+
+func messageCreate(s *discordgo.Session, event *discordgo.MessageCreate) {
+	if strings.HasPrefix(event.Content, c.Prefix) {
 		if event.Author.ID == s.State.User.ID || event.Author.Bot {
 			return
 		}
 
-		msgList := strings.Fields(strings.TrimPrefix(event.Content, "!xd "))
-		command := strings.TrimSpace(msgList[0])
-		channelInGuild, _ := s.Channel(event.ChannelID)
-		guildDetails, _   := s.Guild(channelInGuild.GuildID)
-		roleStruct 		  := guildDetails.Roles
-		submatch := emojiRegex.FindStringSubmatch(msgList[0])
+		msgList := strings.Fields(strings.TrimPrefix(event.Content, c.Prefix))
 
-		//EMOJI 
-		if command == commandList[0] || len(submatch) != 0 {
-			var emojiID string
-			if command == commandList[0] {
-				emojiID = emojiRegex.FindStringSubmatch(msgList[1])[1]
-			}else if len(submatch) != 0 {
-				emojiID = submatch[1]
-			}else {
-				return
-			}
-			h, err := http.Get(fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.png", emojiID)); if err != nil { return }
-			s.ChannelFileSend(event.ChannelID, "emoji.png", h.Body)
-			h.Body.Close()
+		if len(msgList) > 0 {
+			command := strings.TrimSpace(msgList[0])
+			channelInGuild, _ := s.Channel(event.ChannelID)
+			guildDetails, _   := s.Guild(channelInGuild.GuildID)
+			roleStruct 		  := guildDetails.Roles
+			submatch := emojiRegex.FindStringSubmatch(msgList[0])
 
-		//USER STATS			
-		}else if command == commandList[1] {
-			submatch := userIDRegex.FindStringSubmatch(msgList[1])
-
-			var userID string
-			var nick string
-
-			if len(submatch) != 0 { userID = submatch[1] }
-
-			user, error := s.User(userID); if error != nil { return }
-
-			memberStruct, _ := s.State.Member(channelInGuild.GuildID, user.ID)
-
-			var roleNames []string
-
-			for _, role := range memberStruct.Roles {
-				for _, guildRole := range roleStruct {
-					if guildRole.ID == role{
-						roleNames = append(roleNames, guildRole.Name)
+			//EMOJI 
+			if command == commandList[0] || len(submatch) != 0 || emojiFile(msgList[0]) != ""{
+				//if custom emoji
+				if len(submatch) != 0 {
+					var emojiID string
+					if command == commandList[0]{
+						emojiID = emojiRegex.FindStringSubmatch(msgList[1])[1]
+					}else{
+						emojiID = submatch[1]
+					}
+					resp, err := http.Get(fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.png", emojiID)); if err != nil { return }
+					defer resp.Body.Close()					
+					s.ChannelFileSend(event.ChannelID, "emoji.png", resp.Body)
+					s.ChannelMessageDelete(event.ChannelID, event.ID)
+				//elif not custom emoji
+				}else{
+					var name string
+					if command == commandList[0] && len(msgList) > 1 {
+						name = emojiFile(msgList[1])
+					}else{
+						name = emojiFile(msgList[0])
+					}
+					if name != "" {
+						file, err := os.Open(fmt.Sprintf("emoji/%s.png", name)); if err != nil { fmt.Println(err); return }
+						defer file.Close()
+						s.ChannelFileSend(event.ChannelID, "emoji.png", file)
+						s.ChannelMessageDelete(event.ChannelID, event.ID)
 					}
 				}
-			}
 
-			if memberStruct.Nick == "" {
-				nick = "None"
-			}else{
-				nick = memberStruct.Nick
-			}
-			
-			if len(roleNames) == 0 {
-				roleNames = append(roleNames, "None")
-			}
+			//USER STATS			
+			}else if command == commandList[1] {
+				var userID string
+				var nick string
 
-			s.ChannelMessageSendEmbed(event.ChannelID, &discordgo.MessageEmbed{
-					Color:       s.State.UserColor(userID, event.ChannelID),
-					Description: fmt.Sprintf("%s is a loyal member of %s", user.Username, guildDetails.Name),
-					Author: 	 &discordgo.MessageEmbedAuthor{
-						Name:    	user.Username,
-						IconURL: 	discordgo.EndpointUserAvatar(userID, user.Avatar),
-					},
-					Footer: 	 &discordgo.MessageEmbedFooter{
-						Text: 	 	"Brought to you by 2Bot", 
-					},
-					Fields: 	 []*discordgo.MessageEmbedField {
-									&discordgo.MessageEmbedField{Name: "Username:", Value: user.Username, Inline: true},
-									&discordgo.MessageEmbedField{Name: "Nickname:", Value: nick, Inline: true},
-									&discordgo.MessageEmbedField{Name: "Joined Server:", Value: memberStruct.JoinedAt[:10], Inline: false},
-									&discordgo.MessageEmbedField{Name: "Roles:", Value: strings.Join(roleNames, ", "), Inline: true},
-							//		&discordgo.MessageEmbedField{Name: "ID Number:", Value: user.ID, Inline: true},
-								 },
-				})
+				if len(msgList) > 1 {
+					submatch := userIDRegex.FindStringSubmatch(msgList[1])
+					if len(submatch) != 0 { 
+						userID = submatch[1] 
+					}
+				} else {
+					userID = event.Author.ID
+				}
 
-		//SET GAME
-		}else if command == commandList[2] && (event.Author.ID == "149612775587446784" || event.Author.ID == guildDetails.OwnerID) {
-			game := strings.TrimPrefix(event.Content, "!xd setGame ")
-			s.UpdateStatus(0, fmt.Sprintf("%s", game))
+				user, error := s.User(userID); if error != nil { return }
 
-			msg, _ := s.ChannelMessageSend(event.ChannelID, "Game changed successfully!")
+				memberStruct, _ := s.State.Member(channelInGuild.GuildID, user.ID)
 
-			time.Sleep(time.Second*3)
+				var roleNames []string
 
-			err := s.ChannelMessageDelete(event.ChannelID, event.ID); if err != nil{
-        		fmt.Printf("Permission Error: %v\n", err)
-			}
-			s.ChannelMessageDelete(event.ChannelID, msg.ID)
+				for _, role := range memberStruct.Roles {
+					for _, guildRole := range roleStruct {
+						if guildRole.ID == role{
+							roleNames = append(roleNames, guildRole.Name)
+						}
+					}
+				}
 
-			c.Game = game
-			err1 := SaveConfig(); if err1 != nil {
-				fmt.Println(err1)
-			}
+				if memberStruct.Nick == "" {
+					nick = "None"
+				}else{
+					nick = memberStruct.Nick
+				}
+				
+				if len(roleNames) == 0 {
+					roleNames = append(roleNames, "None")
+				}
 
-		//HELP
-		}else if command == commandList[3] {
-			var output []*discordgo.MessageEmbedField
-			for _,item := range commandList{
-				output = append(output, &discordgo.MessageEmbedField{Name: "!xd "+item, Value: item, Inline: false},
-)
-			}
-			s.ChannelMessageSendEmbed(event.ChannelID, &discordgo.MessageEmbed{
-					Color:       000,
+				s.ChannelMessageSendEmbed(event.ChannelID, &discordgo.MessageEmbed{
+						Color:       s.State.UserColor(userID, event.ChannelID),
+						Description: fmt.Sprintf("%s is a loyal member of %s", user.Username, guildDetails.Name),
+						Author: 	 &discordgo.MessageEmbedAuthor{
+							Name:    	user.Username,
+							IconURL: 	discordgo.EndpointUserAvatar(userID, user.Avatar),
+						},
+						Footer: 	 &discordgo.MessageEmbedFooter{
+							Text: 	 	"Brought to you by 2Bot", 
+						},
+						Fields: 	 []*discordgo.MessageEmbedField {
+										&discordgo.MessageEmbedField{Name: "Username:", Value: user.Username, Inline: true},
+										&discordgo.MessageEmbedField{Name: "Nickname:", Value: nick, Inline: true},
+										&discordgo.MessageEmbedField{Name: "Joined Server:", Value: memberStruct.JoinedAt[:10], Inline: false},
+										&discordgo.MessageEmbedField{Name: "Roles:", Value: strings.Join(roleNames, ", "), Inline: true},
+								//		&discordgo.MessageEmbedField{Name: "ID Number:", Value: user.ID, Inline: true},
+									},
+					})
+
+			//SET GAME
+			}else if command == "setGame" && (event.Author.ID == "149612775587446784") {
+				game := strings.TrimPrefix(event.Content, c.Prefix+" setGame ")
+				s.UpdateStatus(0, fmt.Sprintf("%s", game))
+
+				msg, _ := s.ChannelMessageSend(event.ChannelID, ":ok_hand: | Game changed successfully!")
+
+				time.Sleep(time.Second*5)
+
+				s.ChannelMessageDelete(event.ChannelID, event.ID)
+				s.ChannelMessageDelete(event.ChannelID, msg.ID)
+
+				c.Game = game
+				err1 := saveConfig(); if err1 != nil {
+					fmt.Println(err1)
+				}
+
+			//HELP
+			}else if command == commandList[2] {
+				var output []*discordgo.MessageEmbedField
+				for _,item := range helpKeys{
+					output = append(output, &discordgo.MessageEmbedField{Name: c.Prefix+" "+item, Value: helpText[item], Inline: false})
+				}
+				s.ChannelMessageSendEmbed(event.ChannelID, &discordgo.MessageEmbed{
+						Color:       0,
+						Author: 	 &discordgo.MessageEmbedAuthor{
+							Name:    	s.State.User.Username,
+							IconURL: 	discordgo.EndpointUserAvatar(s.State.User.ID, s.State.User.Avatar),
+						},
+						Footer: 	 &discordgo.MessageEmbedFooter{
+							Text: 	 	"Brought to you by 2Bot", 
+						},
+						Fields: 	 output,
+					})
+
+			//HARAM AF
+			}else if command == "haf" {
+				img, err := os.Open("images/haram.jpg"); if err != nil { fmt.Println(err) }
+				defer img.Close()
+				s.ChannelFileSend(event.ChannelID, "haramaf.jpg", img)
+
+			//SET PREFIX
+			}else if command == "setPrefix" && (event.Author.ID == "149612775587446784") && len(msgList) > 1{
+				c.Prefix = msgList[1]
+				msg,_ := s.ChannelMessageSend(event.ChannelID, ":ok_hand: | All done! Prefix changed!")
+				err := saveConfig(); if err != nil { fmt.Println(err); return }
+				time.Sleep(time.Second*5)
+
+				s.ChannelMessageDelete(event.ChannelID, event.ID)
+				s.ChannelMessageDelete(event.ChannelID, msg.ID)
+
+			} else if command == commandList[3] {
+				if len(msgList) > 1 {
+					s.ChannelTyping(event.ChannelID)
+					var query string
+					for _, word := range msgList[1:] {
+						query += "+"+word
+					}
+					resp, err := http.Get(fmt.Sprintf("https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=%s",query)); if err != nil { return }
+					defer resp.Body.Close()
+					body, err1 := ioutil.ReadAll(resp.Body)	
+					if err1 != nil {
+						fmt.Println(err1)
+						return
+					}
+
+					err2 := xml.Unmarshal(body, &r34)
+					if err2 != nil {
+						fmt.Println(err2);
+						return
+					}
+
+					var url string
+					if r34.PostCount == 0 {
+						s.ChannelMessageSend(event.ChannelID, "No results ¯\\_(ツ)_/¯")
+					} else {	
+						url = "https:"+r34.Posts[randRange(0,len(r34.Posts)-1)].URL
+						resp1, err2 := http.Get(url)
+						if err2 != nil { 
+							return 
+						}
+						
+						defer resp1.Body.Close()						
+						s.ChannelMessageSend(event.ChannelID, fmt.Sprintf("%s searched for `%s` \n%s", event.Author.Username, strings.Replace(query, "+"," ",-1), url))
+						//resets list of URLs
+						r34.Posts = r34.Posts[:0]
+					}
+				}
+				return
+
+			}else if command == commandList[4]{
+				ct1,_ := getCreationTime(s.State.User.ID)
+				creationTime := ct1.Format(time.UnixDate)[:10]
+
+				s.ChannelMessageSendEmbed(event.ChannelID, &discordgo.MessageEmbed{
+					Color:       0,
 					Author: 	 &discordgo.MessageEmbedAuthor{
 						Name:    	s.State.User.Username,
 						IconURL: 	discordgo.EndpointUserAvatar(s.State.User.ID, s.State.User.Avatar),
 					},
 					Footer: 	 &discordgo.MessageEmbedFooter{
-						Text: 	 	"Brought to you by 2Bot", 
+						Text: 	 	"Brought to you by 2Bot. Last Bot reboot: " + lastReboot+ " GMT", 
 					},
-					Fields: 	 output,
+					Fields: 	 []*discordgo.MessageEmbedField {
+									&discordgo.MessageEmbedField{Name: "Bot Name:", Value: codeBlock(s.State.User.Username), Inline: true},
+									&discordgo.MessageEmbedField{Name: "Creation Date:", Value: codeBlock(creationTime), Inline: true},
+									&discordgo.MessageEmbedField{Name: "Creator:", Value: "```Strum355#1180```", Inline: true},
+									&discordgo.MessageEmbedField{Name: "Programming Language", Value: "```Go```", Inline: true},
+									&discordgo.MessageEmbedField{Name: "Library", Value: "```Discordgo```", Inline: true},	
+									&discordgo.MessageEmbedField{Name: "Server Count", Value: codeBlock(strconv.Itoa(len(s.State.Guilds))), Inline: true},								
+								},					
+					})
+			}else if command == commandList[5]{
+				doc, err := goquery.NewDocument(fmt.Sprintf("https://ibsearch.xxx/api/v1/images.html?key=2480CFA681A7A882CB33C0E4BA00A812C6F906A6&q=%s&random:", msgList[1]))
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				prev := 1
+				doc.Find("table tr").Each(func(_ int,tr *goquery.Selection)  {
+					// For each <tr> found, find the <td>s inside
+					tr.Find("td[colspan*=\"3\"]").Each(func(_ int, td *goquery.Selection){
+						if (strings.HasSuffix(td.Text(), ".gif") || strings.HasSuffix(td.Text(), ".png") || strings.HasSuffix(td.Text(), ".jpg")) && prev == 1 {
+							prev++
+							//test++
+							//fmt.Println(fmt.Sprintf("Fetching https://im1.ibsearch.xxx/%s", td.Text()))	
+							s.ChannelMessageSend(event.ChannelID, fmt.Sprintf("https://im1.ibsearch.xxx/%s", td.Text()))	
+						}
+					})
 				})
-		//RESPONSES
-		}else{
-			keyWord := reply[strings.TrimSpace(msgList[0])]
-			if keyWord != "" {
-				s.ChannelMessageSend(event.ChannelID, keyWord)
 			}
 		}
 	}
+	return
 }
 
 func joined(s *discordgo.Session, event *discordgo.GuildCreate) {
@@ -233,7 +419,7 @@ func joined(s *discordgo.Session, event *discordgo.GuildCreate) {
 }
 
 func online(s *discordgo.Session, event *discordgo.Ready){
-	LoadConfig(s)
+	loadConfig(s)
 }
 
 func membPresChange(s *discordgo.Session, event *discordgo.PresenceUpdate) {
