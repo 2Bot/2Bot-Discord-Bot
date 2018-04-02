18c13a952d2fd2f3e3dbde8a4bacd0db9f439c4c
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-04-02 19:47:05 +0100

    More reorganzing. Of ibsearch. cant test coz site is down

--- msgIbsearch.go
+++ msgIbsearch.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"time"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
@@ -17,14 +17,17 @@ func init() {
 }
 
 func msgIbsearch(s *discordgo.Session, m *discordgo.MessageCreate, msglist []string) {
-	guild, err := guildDetails(m.ChannelID, "", s)
+	if len(msglist) < 2 {
+		return
+	}
+	
+	channel, err := channelDetails(m.ChannelID, s)
 	if err != nil {
 		return
 	}
 
-	channel, err := s.State.Channel(m.ChannelID)
+	guild, err := guildDetails("", channel.GuildID, s)
 	if err != nil {
-		errorLog.Println("Channel error", err)
 		return
 	}
 
@@ -32,30 +35,29 @@ func msgIbsearch(s *discordgo.Session, m *discordgo.MessageCreate, msglist []str
 		s.ChannelMessageSend(m.ChannelID, "NSFW is disabled on this server~")
 		return
 	}
-
-	if len(msglist) < 2 {
-		return
-	}
-
-	ibsearchStruct := ibStruct{}
-	queryList := strings.Split(strings.Join(remove(msglist, 0), " "), "|")
-	finalQuery := " "
-	filters := []string{"rating", "format"}
-	queries := []string{}
+	
+	s.ChannelTyping(m.ChannelID)
+	
 	URL, err := url.Parse("https://ibsearch.xxx")
 	if err != nil {
 		errorLog.Println("IBSearch query error", err)
 		return
 	}
-
-	s.ChannelTyping(m.ChannelID)
-
+	
+	var queries []string
+	
+	queryList := strings.Split(strings.Join(remove(msglist, 0), " "), "|")
+	
 	for i, item := range queryList {
 		if strings.Contains(item, "=") {
 			queries = append(queries, strings.TrimSpace(strings.Split(queryList[i], "=")[0]))
 		}
 	}
-
+	
+	var finalQuery string	
+	
+	filters := []string{"rating", "format"}
+	
 	for _, item1 := range filters {
 		for i, item2 := range queries {
 			if strings.Contains(item1, item2) {
@@ -63,40 +65,38 @@ func msgIbsearch(s *discordgo.Session, m *discordgo.MessageCreate, msglist []str
 			}
 		}
 	}
-
+	
 	//Assemble the URL
+	var par url.Values
 	URL.Path += "/api/v1/images.json"
-	par := url.Values{}
-	par.Add("q", strings.TrimSpace(queryList[0])+finalQuery+"random:")
+	par.Add("q", strings.TrimSpace(queryList[0])+" "+finalQuery+"random:")
 	par.Add("limit", "1")
+	
 	//Public key that is for free, worst that can happen is that
 	//i hit the ratelimit, but please dont do that to me
 	par.Add("key", "2480CFA681A7A882CB33C0E4BA00A812C6F906A6")
 	URL.RawQuery = par.Encode()
-
-	page, err := http.Get(URL.String())
+	
+	client := http.Client{Timeout: time.Second * 2}
+	page, err := client.Get(URL.String())
 	if err != nil {
 		errorLog.Println("Ibsearch http error", err)
+		s.ChannelMessageSend(m.ChannelID, "Error getting results from ibsearch")
+		return
 	}
+	defer page.Body.Close()
+	
 	if page.StatusCode != http.StatusOK {
 		s.ChannelMessageSend(m.ChannelID, "IBSearch didn't respond :(")
 		return
 	}
-	defer page.Body.Close()
-
-	body, err := ioutil.ReadAll(page.Body)
-	if err != nil {
-		errorLog.Println("IBSearch response body err:", err)
-		return
-	}
-
-	err = json.Unmarshal([]byte(strings.TrimPrefix(strings.TrimSuffix(string(body), "]"), "[")), &ibsearchStruct)
-	if err != nil {
+	
+	var ibsearchStruct [1]ibStruct
+	if err := json.NewDecoder(page.Body).Decode(ibsearchStruct); err != nil {
 		errorLog.Println("IBSearch json unmarshal err:", err)
 		s.ChannelMessageSend(m.ChannelID, "No results ¯\\_(ツ)_/¯")
 		return
 	}
 
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s searched for `%s` \nhttps://%s.ibsearch.xxx/%s", m.Author.Username, queryList[0], ibsearchStruct.Server, ibsearchStruct.Path))
-	return
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%s searched for `%s` \nhttps://%s.ibsearch.xxx/%s", m.Author.Username, queryList[0], ibsearchStruct[0].Server, ibsearchStruct[0].Path))
 }
